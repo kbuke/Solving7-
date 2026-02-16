954f94d07bd3bb612b998cdc836545866f88c904
Kaan Buke <kabuke13@gmail.com>
2026-02-16 15:04:18 +0200

    initial commit

---
+++ client/src/router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+
+import App from "./App";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            // Create object with path and element keys 
+        ]
+    }
+])
